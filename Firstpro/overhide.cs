873defe53807127a7be59675a32dfa29ddb5da23
rohanhk791 <rohanhk2@gmail.com>
2022-05-27 17:10:16 +0530

    commit

---
+++ Firstpro/overhide.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firstpro
+{
+    class overhide
+    {
+        public void display()
+        {
+            Console.WriteLine("Super class  method");
+        }
+    }
+    class Class2 : overhide
+    {
+        public new void display()
+        {
+            Console.WriteLine("Sub class  method");
+        }
+    }
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            overhide obj = new Class2();
+            obj.display();
+            Console.ReadKey();
+        }
+    }
+}
