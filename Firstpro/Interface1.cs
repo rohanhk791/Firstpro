e774dd1fb53920e154020a378b1dddff82c695ea
rohanhk791 <rohanhk2@gmail.com>
2022-05-31 10:17:06 +0530

    c

---
+++ Firstpro/Interface1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firstpro
+{
+    interface Interface1
+    {
+        void add(int a, int b);
+    }
+    interface inter : Interface1
+    { 
+        void sub(int a, int b);
+
+    }
+    class imp : inter
+    {
+        public void add(int a,int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        public void sub(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+    
+    static void Main()
+        {
+            imp obj = new imp();
+            obj.add(10, 20);
+            obj.sub(30, 10);
+            Console.ReadLine();
+        }
+    
+    }
+
+
+
+
+
+}
+
---
+++ Firstpro/getset.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Firstpro
+{
+    class getset
+    {
+        private string SName;
+        public getset(string name) => SName = name;
+        public string Name
+        {
+            get => SName;          
+            set => SName = value;    
+        }
+    }
+    class getsetprogram
+    {
+        public static void Main()
+        {
+            getset stu = new getset("rohan gowda");
+            Console.WriteLine(stu.Name);
+        }
+    }
+
+}
+
